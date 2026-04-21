e16ef80bea91df8e7b224ea53f93f7976710a37d
sandyshd <sandip.p.007@gmail.com>
2026-04-20 19:58:58 -0500

    fix: change default region to eastus2 to avoid Cosmos DB capacity limits

--- infra/variables.tf
+++ infra/variables.tf
@@ -18,7 +18,7 @@ variable "environment" {
 variable "location" {
   description = "Azure region for all resources"
   type        = string
-  default     = "eastus"
+  default     = "eastus2"
 }
 
 variable "openai_location" {
